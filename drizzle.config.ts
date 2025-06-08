68ec0564d1b5c1bde9ae582252eee965c3d54ed7
Ayush Kumar <ayush23162@iiitd.ac.in>
2025-06-08 20:40:03 +0530

    Init

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["e3-chat_*"],
+} satisfies Config;
